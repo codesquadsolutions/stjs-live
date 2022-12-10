31a7b39788ef2e064c2af50709bed63d1f587827
codesquadsolutions <codesquadsolutions@gmail.com>
2022-12-10 13:44:55 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { environment } from 'src/environments/environment.prod';
+import { enableProdMode } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    initializeApp(environment.firebaseConfig);
+    enableProdMode();
+  }
 }
+
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBozkoUdwE3WrNrjDKyAfg4I2hOXoE9nLo",
+    authDomain: "stjs-live.firebaseapp.com",
+    projectId: "stjs-live",
+    storageBucket: "stjs-live.appspot.com",
+    messagingSenderId: "274790605258",
+    appId: "1:274790605258:web:912a21eb178d40a286ef33",
+    measurementId: "G-73FCN710J4"
+  }
+  
 };
